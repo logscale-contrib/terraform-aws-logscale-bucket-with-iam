26819feaf9e671ee927c079cf933b87e42a36e99
Ryan Faircloth <ryan@dss-i.com>
2022-10-21 07:41:30 -0400

    feat: create bucket

--- main.tf
+++ main.tf
@@ -49,3 +49,44 @@ module "kms" {
   tags = local.tags
 
 }
+
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "3.4.0"
+
+  attach_deny_insecure_transport_policy = true
+  bucket_prefix                         = "logscale-${local.env}-ops"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        kms_master_key_id = module.kms.key_arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+  versioning = {
+    status = true
+  }
+  intelligent_tiering = {
+    logscale = {
+      status = "Enabled"
+      filter = {
+        prefix = "/"
+      }
+      tiering = {
+        ARCHIVE_ACCESS = {
+          days = 90
+        }
+        DEEP_ARCHIVE_ACCESS = {
+          days = 180
+        }
+      }
+    }
+  }
+}
