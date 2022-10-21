956d2c3398b01a382346fb39bc826e473bd6aebe
Ryan Faircloth <ryan@dss-i.com>
2022-10-21 08:48:49 -0400

    feat: add outputs

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "iam_role_arn" {
+    value = module.irsa.iam_role_arn
+}
+output "s3_bucket_arn" {
+    value = module.s3-bucket.s3_bucket_arn
+}
+output "s3_bucket_id" {
+    value = module.s3-bucket.s3_bucket_id
+}
